a54b6b42ccfeab6f7d8b18b019d14aab66242af9
npokrandt <npokrandt919@gmail.com>
2023-06-24 22:00:40 -0500

    storage experimentation and answers can detect being clicked

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,7 +14,7 @@
 
 //alert("test")
   
-// localStorage.setItem("questions", JSON.stringify([]))
+//localStorage.removeItem("test2")
 
 // function buildQuestion(prompt, randomize, possibleAnswers, correctAnswer){
 //     var question = {
@@ -27,132 +27,155 @@
 //     return question
 // }
 
-var header = document.querySelector("h1")
-var startBtn = document.getElementById("start-quiz")
-var startPage = document.getElementById("start-page")
-var body = document.body
-var timerEl = document.getElementById("time-remaining")
+// var array = JSON.parse(localStorage.getItem("questions"))
 
-var secondsLeft = 90
-var timer
 
-var questionCount = 1
+// array.push(buildQuestion("Which of the following is not a primitive data type in JS?", true, ['boolean', 'float', 'symbol', 'undefined'], 'float'))
+// console.log(array)
 
-// Pseudocode
-
-//user clicks start button/restart button
+// if (array === null){
+//     console.log("the array has not been added yet")
+// } else {
+    //     console.log("array already exists")
+    // }
+    
+    // localStorage.setItem("questions", JSON.stringify(array))
+    
+    var header = document.querySelector("h1")
+    var startBtn = document.getElementById("start-quiz")
+    var startPage = document.getElementById("start-page")
+    var body = document.body
+    var timerEl = document.getElementById("time-remaining")
+    
+    var secondsLeft = 90
+    var timer
+    
+    var questionCount = 1
+    
+    // Pseudocode 
+    
+    //user clicks start button/restart button
     //a random question is pulled from storage
     //it is sent to the DOM
     //the timer is started
-
-//user clicks answer option
+    
+    //user clicks answer option
     //if the answer is correct
-        //remove old question
+    //remove old question
         //if it is the last question
             //prompt user for initials if a high score is set
             //show end game screen
-        //else
+            //else
             //add new question
-    //else 
-        //deduct time from the timer
-        //inform user that the answer is wrong
-
-function startGame(){
-    console.log("Game started")
-    startPage.style.display = "none"
-    header.innerText = "."
-    header.style.color = "lightpink"
-    header.style.cursor = "default"
-    timer = setInterval(tick, 1000)
-    //build the question
-    var question = "Which of the following is not a primitive data type in JS?"
-    var options = ["boolean", "float", "symbol", "undefined"]
-    buildQuestion(question, options)
-}
-
-function buildQuestion(question, answers){
-    var questionDiv = document.createElement("div")
-    questionDiv.className = "question-container"
-    body.appendChild(questionDiv)
-
-    var parent = document.querySelector(".question-container")
-
+            //else 
+            //deduct time from the timer
+            //inform user that the answer is wrong
+            
+            function startGame(){
+                console.log("Game started")
+                startPage.style.display = "none"
+                header.innerText = "."
+                header.style.color = "lightpink"
+                header.style.cursor = "default"
+                timer = setInterval(tick, 1000)
+                //build the question
+                var question = JSON.parse(localStorage.getItem("questions"))
+                var questionPrompt = question[0].prompt
+                var options = question[0].possibleAnswers
+                buildQuestion(questionPrompt, options)
+            }
+            
+            function buildQuestion(question, answers){
+                var questionDiv = document.createElement("div")
+                questionDiv.className = "question-container"
+                body.appendChild(questionDiv)
+                
+                var parent = document.querySelector(".question-container")
+                
     var questionCountLabel = document.createElement("h3")
     questionCountLabel.innerHTML = "Question <span>1</span>/10"
     questionCountLabel.id = "question-count"
     parent.appendChild(questionCountLabel)
-
+    
     var questionLabel = document.createElement("h4")
     questionLabel.innerText = question
     questionLabel.id = "question"
     parent.appendChild(questionLabel)
-
+    
     var answerOptions = document.createElement("ol")
     answerOptions.id = "answers"
     parent.appendChild(answerOptions)
-
+    
     addAnswer(answers[0], "one")
     addAnswer(answers[1], "two")
     addAnswer(answers[2], "three")
     addAnswer(answers[3], "four")
-
+    
 }
 
 function addAnswer(answer, answerClass){
     var parent = document.querySelector("ol")
-
+    
     var answerOption = document.createElement("li")
     answerOption.innerText = answer
     answerOption.className = answerClass
     parent.appendChild(answerOption)
 }
-    
+
 function tick(){
     secondsLeft--
     timerEl.innerText = secondsLeft
-    console.log(secondsLeft)
+    //console.log(secondsLeft)
     if (secondsLeft === 0){
         clearInterval(timer)
     }
 }
 
+function submitAnswer(event){
+    if(event.target.matches("li")){
+        console.log("possible answer")
+    }
+    
+}
+
 
 startBtn.addEventListener("click", startGame)
+body.addEventListener("click", submitAnswer)
 //sample questions for the quiz
 
 // structure: could be an object storing both prompt and array
 // will be several of these to choose from at random
 
-//Which of the following is not a primitive data type in JS?
-    //boolean
-    //symbol
-    //float
-    //undefined
+// Which of the following is not a primitive data type in JS?
+// boolean
+// symbol
+// float
+// undefined
 
 //which of the following isn't a keyword to declare a variable?
-    //let
-    //init
-    //var
-    //const
+//let
+//init
+//var
+//const
 
 //which of the following is not a correct statement in a for loop in JS?
-    //int i = 0;
-    //i <= 17;
-    //i+=7;
-    //i--
+//int i = 0;
+//i <= 17;
+//i+=7;
+//i--
 
 //what property allows you to get the number of elements in an array?
-    //amount
-    //size
-    //length
-    //includes
+//amount
+//size
+//length
+//includes
 
 //what can an object take as values?
     //strings
     //arrays
     //other objects
     //all of the above
-
+    
 //how does the HTML file read the JS file?
     //<script>assets/js/script.js</script>
     //<script href="assets/js/script.js"></script>
