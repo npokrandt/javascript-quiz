108db5f23ef4c3d15ef445b36276722d47cf51db
npokrandt <npokrandt919@gmail.com>
2023-06-26 17:51:30 -0500

    modified some of the js and HTML, and answers can detect if they are correct or not

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,22 @@
 //the criteria
 
-// GIVEN I am taking a code quiz - yay
-// WHEN I click the start button - does nothing yet
-// THEN a timer starts and I am presented with a question - don't even have a timer
-// WHEN I answer a question - not there yet
-// THEN I am presented with another question
+// GIVEN I am taking a code quiz
+// WHEN I click the start button 
+// THEN a timer starts and I am presented with a question - check
+// WHEN I answer a question 
+// THEN I am presented with another question - coming soon
 // WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
+// THEN time is subtracted from the clock - pending
 // WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
+// THEN the game is over - pending
 // WHEN the game is over
-// THEN I can save my initials and my score
+// THEN I can save my initials and my score - pending
 
-//alert("test")
-  
-//localStorage.removeItem("test2")
+// array.push(buildQuestion("How does the HTML file read the JS file?", false, ['<script>assets/js/script.js</script>', '<script href="assets/js/script.js"></script>', '<link href="assets/js/script.js"></script>', 'none of the above'], 'none of the above'))
+// array.push(buildQuestion("Which variable name would work in JS?", true, ['user-name', 'user_name', '1User', '!user'], 'user_name'))
+// array.push(buildQuestion("What can be stored in arrays?", false, ['strings', 'null', 'other arrays', 'all of the above'], 'all of the above'))
+// array.push(buildQuestion("Which of the following is the AND operator?", true, ['||', '@@', '&&', '??'], '&&'))   
+// array.push(buildQuestion("Which is not a method of Math?", true, ['random()', 'ceiling()', 'abs()', 'hypot()'], 'ceiling()'))
 
 // function buildQuestion(prompt, randomize, possibleAnswers, correctAnswer){
 //     var question = {
@@ -23,129 +25,9 @@
 //         possibleAnswers: possibleAnswers,
 //         correctAnswer: correctAnswer
 //     }
-    
+
 //     return question
 // }
-
-// var array = JSON.parse(localStorage.getItem("questions"))
-
-
-// array.push(buildQuestion("Which of the following is not a primitive data type in JS?", true, ['boolean', 'float', 'symbol', 'undefined'], 'float'))
-// console.log(array)
-
-// if (array === null){
-//     console.log("the array has not been added yet")
-// } else {
-    //     console.log("array already exists")
-    // }
-    
-    // localStorage.setItem("questions", JSON.stringify(array))
-    
-    var header = document.querySelector("h1")
-    var startBtn = document.getElementById("start-quiz")
-    var startPage = document.getElementById("start-page")
-    var body = document.body
-    var timerEl = document.getElementById("time-remaining")
-    
-    var secondsLeft = 90
-    var timer
-    
-    var questionCount = 1
-    
-    // Pseudocode 
-    
-    //user clicks start button/restart button
-    //a random question is pulled from storage
-    //it is sent to the DOM
-    //the timer is started
-    
-    //user clicks answer option
-    //if the answer is correct
-    //remove old question
-        //if it is the last question
-            //prompt user for initials if a high score is set
-            //show end game screen
-            //else
-            //add new question
-            //else 
-            //deduct time from the timer
-            //inform user that the answer is wrong
-            
-            function startGame(){
-                console.log("Game started")
-                startPage.style.display = "none"
-                header.innerText = "."
-                header.style.color = "lightpink"
-                header.style.cursor = "default"
-                timer = setInterval(tick, 1000)
-                //build the question
-                var question = JSON.parse(localStorage.getItem("questions"))
-                var questionPrompt = question[0].prompt
-                var options = question[0].possibleAnswers
-                buildQuestion(questionPrompt, options)
-            }
-            
-            function buildQuestion(question, answers){
-                var questionDiv = document.createElement("div")
-                questionDiv.className = "question-container"
-                body.appendChild(questionDiv)
-                
-                var parent = document.querySelector(".question-container")
-                
-    var questionCountLabel = document.createElement("h3")
-    questionCountLabel.innerHTML = "Question <span>1</span>/10"
-    questionCountLabel.id = "question-count"
-    parent.appendChild(questionCountLabel)
-    
-    var questionLabel = document.createElement("h4")
-    questionLabel.innerText = question
-    questionLabel.id = "question"
-    parent.appendChild(questionLabel)
-    
-    var answerOptions = document.createElement("ol")
-    answerOptions.id = "answers"
-    parent.appendChild(answerOptions)
-    
-    addAnswer(answers[0], "one")
-    addAnswer(answers[1], "two")
-    addAnswer(answers[2], "three")
-    addAnswer(answers[3], "four")
-    
-}
-
-function addAnswer(answer, answerClass){
-    var parent = document.querySelector("ol")
-    
-    var answerOption = document.createElement("li")
-    answerOption.innerText = answer
-    answerOption.className = answerClass
-    parent.appendChild(answerOption)
-}
-
-function tick(){
-    secondsLeft--
-    timerEl.innerText = secondsLeft
-    //console.log(secondsLeft)
-    if (secondsLeft === 0){
-        clearInterval(timer)
-    }
-}
-
-function submitAnswer(event){
-    if(event.target.matches("li")){
-        console.log("possible answer")
-    }
-    
-}
-
-
-startBtn.addEventListener("click", startGame)
-body.addEventListener("click", submitAnswer)
-//sample questions for the quiz
-
-// structure: could be an object storing both prompt and array
-// will be several of these to choose from at random
-
 // Which of the following is not a primitive data type in JS?
 // boolean
 // symbol
@@ -182,26 +64,180 @@ body.addEventListener("click", submitAnswer)
     //<link href="assets/js/script.js"></script>
     //none of the above
 
-//what can be stored in arrays?
+    
+    //What can be stored in arrays?
     //strings
     //null
     //other arrays
     //all of the above
 
-//which is not a method of Math?
+    
+    //which is not a method of Math?
     //random()
     //ceiling()
     //abs()
     //hypot()
 
-//which of the following is the AND operator?
+    
+    //which of the following is the AND operator?
     //||
     //&&
     //@@
     //??
-
-//which variable name would work in JS?
+    
+    
+    //which variable name would work in JS?
     //user-name
     //user_name
     //1User
-    //name
+    //!user
+    
+    
+    //console.log(array.length)
+    
+    //array.pop()
+    
+    
+    //console.log(array[4])
+    
+    // if (array === null){
+        //     console.log("the array has not been added yet")
+// } else {
+    //     console.log("array already exists")
+    // }
+    
+    //localStorage.setItem("questions", JSON.stringify(array))
+    
+    //main code starts here
+    var header = document.querySelector("h1")
+    var startBtn = document.getElementById("start-quiz")
+    var startPage = document.getElementById("start-page")
+    var questionContainer = document.getElementById("question-container")
+    var timerEl = document.getElementById("time-remaining")
+    var array = JSON.parse(localStorage.getItem("questions"))
+    
+    var secondsLeft = 90
+    var timer
+    
+    var questionCount = 1
+    
+    // Pseudocode 
+    
+    //user clicks start button/restart button
+    //a random question is pulled from storage
+    //it is sent to the DOM
+    //the timer is started
+    
+    //user clicks answer option
+    //if the answer is correct
+    //remove old question
+        //if it is the last question
+            //prompt user for initials if a high score is set
+            //show end game screen
+            //else
+            //add new question
+            //else 
+            //deduct time from the timer
+            //inform user that the answer is wrong
+            
+function startGame(){
+    console.log("Game started")
+    startPage.style.display = "none"
+    //hides the header without messing with the 
+    header.innerText = "."
+    header.style.color = "lightpink"
+    header.style.cursor = "default"
+    timer = setInterval(tick, 1000)
+    //build the question
+    buildQuestion()
+}
+            
+function buildQuestion(){
+
+    //the question elements
+    var question = JSON.parse(localStorage.getItem("questions"))
+    var questionPrompt = question[3].prompt
+    var options = question[3].possibleAnswers
+    var correctAnswer = question[3].correctAnswer
+                
+    var parent = questionContainer
+                
+    var questionCountLabel = document.createElement("h3")
+    questionCountLabel.innerHTML = "Question <span id='question-number'>1</span>/10"
+    questionCountLabel.id = "question-count"
+    parent.appendChild(questionCountLabel)
+    
+    var questionLabel = document.createElement("h4")
+    questionLabel.innerText = questionPrompt
+    questionLabel.id = "question"
+    parent.appendChild(questionLabel)
+    
+    var answerOptions = document.createElement("ol")
+    answerOptions.id = "answers"
+    parent.appendChild(answerOptions)
+
+    for (var i = 0; i < 4; i++){
+        var className
+        var isCorrect = false
+        switch (i) {
+            case 0:
+                className = "one"
+                break;
+            case 1:
+                className = "two"
+                break;  
+            case 2:
+                className = "three"
+                break; 
+            case 3:
+                className = "four"
+                break;          
+        }
+            
+        if (options[i] === correctAnswer){
+            isCorrect = true
+        }
+        addAnswer(options[i], className, isCorrect)
+    }
+    
+    // addAnswer(answers[1], "two")
+    // addAnswer(answers[2], "three")
+    // addAnswer(answers[3], "four")
+    
+}
+
+function addAnswer(answer, answerClass, correctness){
+    var parent = document.querySelector("ol")
+    
+    var answerOption = document.createElement("li")
+    answerOption.innerText = answer
+    answerOption.className = answerClass
+    answerOption.setAttribute("data-correct", correctness);
+    parent.appendChild(answerOption)
+}
+
+function tick(){
+    secondsLeft--
+    timerEl.innerText = secondsLeft
+    //console.log(secondsLeft)
+    if (secondsLeft === 0){
+        clearInterval(timer)
+    }
+}
+
+function submitAnswer(event){
+    if(event.target.matches("li")){
+        var answer = event.target.getAttribute("data-correct")
+        console.log(answer)
+    }
+    
+}
+
+
+startBtn.addEventListener("click", startGame)
+questionContainer.addEventListener("click", submitAnswer)
+//sample questions for the quiz
+
+// structure: could be an object storing both prompt and array
+// will be several of these to choose from at random
+
